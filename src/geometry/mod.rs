4310ad488697100d7373bede95d6410f36967a60
Chris Kapp <chris.kapp@sensat.co.uk>
2025-09-19 16:35:10 +0100

    Add iter_vertices for triangles (#28)

--- src/geometry/mod.rs
+++ src/geometry/mod.rs
@@ -184,6 +184,7 @@ impl LinePrimitive {
                     return Box::new(std::iter::empty());
                 }
 
+                // ignore vertices that do not form a whole line
                 let safe_index_length = if index.len() % 2 == 0 {
                     index.len()
                 } else {
@@ -201,6 +202,8 @@ impl LinePrimitive {
                 if self.vertices.len() == 0 {
                     return Box::new(std::iter::empty());
                 }
+
+                // ignore vertices that do not form a whole line
                 let safe_vertex_length = if self.vertices.len() % 2 == 0 {
                     self.vertices.len()
                 } else {
@@ -223,6 +226,53 @@ impl TrianglePrimitive {
             vertices: Vec::new(),
             indices: None,
             bounding_sphere: Sphere::new(Vec3::new(0.0, 0.0, 0.0), 0.0),
+        }
+    }
+
+    pub fn iter_vertices(
+        &self,
+    ) -> Box<dyn Iterator<Item = (&[f32; 3], &[f32; 3], &[f32; 3])> + '_> {
+        match &self.indices {
+            Some(index) => {
+                if index.len() == 0 {
+                    return Box::new(std::iter::empty());
+                }
+
+                // ignore vertices that do not form a whole triangle
+                let excess_indices = index.len() % 3;
+                let safe_index_length = if excess_indices == 0 {
+                    index.len()
+                } else {
+                    index.len() - excess_indices
+                };
+
+                return Box::new(index[..safe_index_length].chunks(3).map(|chunk| {
+                    (
+                        &self.vertices[chunk[0] as usize],
+                        &self.vertices[chunk[1] as usize],
+                        &self.vertices[chunk[2] as usize],
+                    )
+                }));
+            }
+            None => {
+                if self.vertices.len() == 0 {
+                    return Box::new(std::iter::empty());
+                }
+
+                // ignore vertices that do not form a whole triangle
+                let excess_vertices = self.vertices.len() % 3;
+                let safe_vertex_length = if excess_vertices == 0 {
+                    self.vertices.len()
+                } else {
+                    self.vertices.len() - excess_vertices
+                };
+
+                return Box::new(
+                    self.vertices[..safe_vertex_length]
+                        .chunks(3)
+                        .map(|chunk| (&chunk[0], &chunk[1], &chunk[2])),
+                );
+            }
         }
     }
 }
@@ -296,6 +346,57 @@ mod tests {
         // repeated index should be returned as expected
         assert_eq!(iter.next(), Some((&[2.0, 3.0, 4.0], &[1.0, 2.0, 3.0])));
         // last vertex should be dropped as no valid index pair
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn test_iter_triangle_vertices() {
+        let mut primitive = TrianglePrimitive::new();
+        primitive.set_vertices(vec![
+            [0.0, 1.0, 2.0], // tri 1 a
+            [1.0, 2.0, 3.0], // tri 1 b
+            [2.0, 3.0, 4.0], // tri 1 c
+            [3.0, 4.0, 5.0], // tri 2 a
+            [4.0, 5.0, 6.0], // tri 2 b
+            [5.0, 6.0, 7.0], // tri 2 c
+            [6.0, 7.0, 8.0], // discarded, not a whole triangle
+            [7.0, 8.0, 9.0], // discarded, not a whole triangle
+        ]);
+
+        let mut iter = primitive.iter_vertices();
+        assert_eq!(
+            iter.next(),
+            Some((&[0.0, 1.0, 2.0], &[1.0, 2.0, 3.0], &[2.0, 3.0, 4.0]))
+        );
+        assert_eq!(
+            iter.next(),
+            Some((&[3.0, 4.0, 5.0], &[4.0, 5.0, 6.0], &[5.0, 6.0, 7.0]))
+        );
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn test_iter_triangle_vertices_with_indices() {
+        let mut primitive = TrianglePrimitive::new();
+        primitive.set_vertices(vec![
+            [0.0, 1.0, 2.0],
+            [1.0, 2.0, 3.0],
+            [2.0, 3.0, 4.0],
+            [3.0, 4.0, 5.0],
+            [4.0, 5.0, 6.0],
+        ]);
+        // note: triangles ABC, DEF, then 2 discarded entries
+        primitive.set_indices(vec![0, 2, 1, 3, 2, 1, 4, 5]);
+
+        let mut iter = primitive.iter_vertices();
+        assert_eq!(
+            iter.next(),
+            Some((&[0.0, 1.0, 2.0], &[2.0, 3.0, 4.0], &[1.0, 2.0, 3.0]))
+        );
+        assert_eq!(
+            iter.next(),
+            Some((&[3.0, 4.0, 5.0], &[2.0, 3.0, 4.0], &[1.0, 2.0, 3.0]))
+        );
         assert_eq!(iter.next(), None);
     }
 }
